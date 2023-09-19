87263061d492d3253930d17cd6f88a487540d453
Ruslan Batkaev <rbatkaev@appulate.com>
2023-09-19 11:27:02 +0300

    Add test projects with different execution time

---
+++ Tests4/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Tests4
+{
+    internal class Tests
+    {
+        [Test]
+        public void Sleep1()
+        {
+            Thread.Sleep(1_000);
+        }
+    }
+}
---
+++ Tests6/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Tests6
+{
+    internal class Tests
+    {
+        [Test]
+        public void Sleep1()
+        {
+            Thread.Sleep(10_000);
+        }
+    }
+}
